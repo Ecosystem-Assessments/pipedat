f900ceeb4db01e3d7c2e680cf377220774cd78ed
David Beauchesne <david.beauchesne@uqar.ca>
2022-04-25 16:15:01 -0400

    add study grid maritimes

---
+++ R/dp_maritimes_grid-750b39f9.R
@@ -0,0 +1,88 @@
+#' @eval get_name("750b39f9")
+#'
+#' @eval get_description("750b39f9")
+#'
+#' @eval doc_params()
+#'
+#' @family pipeline functions
+#' @rdname data_pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: 750b39f9
+#'
+#' @examples
+#' \dontrun{
+#' dp_750b39f9()
+#' }
+dp_750b39f9 <- function(output, crs = 4326, bbox = NULL, timespan = NULL, ...) {
+  # Output folders and other objects used
+  uid <- "750b39f9"
+  name <- get_shortname(uid)
+  nm <- glue("{name}-{uid}")
+  output <- make_output(uid, name, output, local = TRUE) # set local = TRUE for local data
+  path <- glue("{output}{nm}/")
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # IMPORT DATA
+  # NOTE: optional
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  dat <- sf::st_read(glue("{path}raw/pu.shp"), quiet = TRUE)
+
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # FORMAT DATA
+  # NOTE: optional
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  UNIT_ID <- geometry <- NULL
+  dat <- sf::st_make_valid(dat)
+  dat <- dplyr::select(dat, ID = UNIT_ID, geometry)
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # CREATE METADATA
+  # WARNING: mandatory
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  meta <- get_metadata(
+    pipeline_id = uid,
+    pipeline_crs = crs,
+    pipeline_bbox = bbox,
+    data_access = timestamp(),
+    data_bbox = sf::st_bbox(dat)
+  )
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # CREATE BIBTEX
+  # WARNING: mandatory
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  bib <- get_bib(uid)
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # APPLY SUBSETS AND CRS SPECIFIED BY USER
+  # NOTE: optional, only if applicable
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  dat <- dp_parameters(
+    dat,
+    crs = crs,
+    bbox = bbox
+  )
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # EXPORT
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Formatted data
+  fm <- glue("{path}/{nm}.geojson") # NOTE: not necessarily spatial data
+  sf::st_write(dat, dsn = fm, quiet = TRUE) # for spatial data
+
+  # Metadata
+  mt <- glue("{path}/{nm}.yaml")
+  yaml::write_yaml(meta, mt, column.major = FALSE)
+
+  # Bibtex
+  bi <- glue("{path}/{nm}.bib")
+  RefManageR::WriteBib(bib, file = bi, verbose = FALSE)
+  # _________________________________________________________________________________________ #
+}

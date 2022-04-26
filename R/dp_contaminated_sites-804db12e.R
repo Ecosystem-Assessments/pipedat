620442c1e2fe17e963767749966e073681df8441
David Beauchesne <david.beauchesne@hotmail.com>
2022-04-26 13:46:48 -0400

    add contaminated sites (#7)

---
+++ R/dp_contaminated_sites-804db12e.R
@@ -0,0 +1,100 @@
+#' @eval get_name("804db12e")
+#'
+#' @eval get_description("804db12e")
+#'
+#' @eval doc_params()
+#'
+#' @family pipeline functions
+#' @rdname data_pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: 804db12e
+#'
+#' @examples
+#' \dontrun{
+#' dp_804db12e()
+#' }
+dp_804db12e <- function(output, crs = 4326, bbox = NULL, timespan = NULL, ...) {
+  # Output folders and other objects used
+  uid <- "804db12e"
+  name <- get_shortname(uid)
+  nm <- glue("{name}-{uid}")
+  output <- make_output(uid, name, output, local = FALSE) # set local = TRUE for local data
+  path <- glue("{output}{nm}/")
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # DOWNLOAD DATA
+  # NOTE: optional
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  urls <- "https://www.tbs-sct.gc.ca/fcsi-rscf/cscsvreport-eng.aspx?cttype=tombstone&qid=752444"
+  pipeload(urls = urls, output = glue("{path}raw/"))
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # IMPORT DATA
+  # NOTE: optional
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  files <- dir(glue("{path}raw/"))
+  suppressMessages(suppressWarnings({
+    dat <- readr::read_csv(glue("{path}raw/{files}"))
+  }))
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # FORMAT DATA
+  # NOTE: optional
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  Longitude <- Latitude <- NULL # for R CMD CHECK
+  dat <- dat |>
+    dplyr::filter(!is.na(Latitude) & !is.na(Longitude)) |>
+    as.data.frame() |>
+    sf::st_as_sf(coords = c("Longitude", "Latitude"), crs = 4269)
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # CREATE METADATA
+  # WARNING: mandatory
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  meta <- get_metadata(
+    pipeline_id = uid,
+    pipeline_crs = crs,
+    pipeline_bbox = bbox,
+    data_access = timestamp(),
+    data_bbox = sf::st_bbox(dat)
+  )
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # CREATE BIBTEX
+  # WARNING: mandatory
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  bib <- get_bib(uid)
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # APPLY SUBSETS AND CRS SPECIFIED BY USER
+  # NOTE: optional, only if applicable
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  dat <- dp_parameters(
+    dat,
+    crs = crs,
+    bbox = bbox
+  )
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # EXPORT
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Formatted data
+  fm <- glue("{path}/{nm}.geojson") # NOTE: not necessarily spatial data
+  sf::st_write(dat, dsn = fm, quiet = TRUE) # for spatial data
+
+  # Metadata
+  mt <- glue("{path}/{nm}.yaml")
+  yaml::write_yaml(meta, mt, column.major = FALSE)
+
+  # Bibtex
+  bi <- glue("{path}/{nm}.bib")
+  RefManageR::WriteBib(bib, file = bi, verbose = FALSE)
+  # _________________________________________________________________________________________ #
+}

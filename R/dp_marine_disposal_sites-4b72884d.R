93e6d8f24464c638016e08db75b97fd05d627bd9
David Beauchesne <david.beauchesne@hotmail.com>
2022-04-26 15:20:58 -0400

    marine disposal sites data pipeline (#8)

---
+++ R/dp_marine_disposal_sites-4b72884d.R
@@ -0,0 +1,100 @@
+#' @eval get_name("4b72884d")
+#'
+#' @eval get_description("4b72884d")
+#'
+#' @eval doc_params()
+#'
+#' @family pipeline functions
+#' @rdname data_pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: 4b72884d
+#'
+#' @examples
+#' \dontrun{
+#' dp_4b72884d()
+#' }
+dp_4b72884d <- function(output, crs = 4326, bbox = NULL, timespan = NULL, ...) {
+  # Output folders and other objects used
+  uid <- "4b72884d"
+  name <- get_shortname(uid)
+  nm <- glue("{name}-{uid}")
+  output <- make_output(uid, name, output, local = FALSE) # set local = TRUE for local data
+  path <- glue("{output}{nm}/")
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # DOWNLOAD DATA
+  # NOTE: optional
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # If the data is downloaded from online sources
+  urls <- c(
+    "http://data.ec.gc.ca/data/sites/assess/active-and-inactive-disposal-at-sea-sites-in-canadian-waters/active-and-inactive-disposal-at-sea-sites-2018/CAN_DAS.gdb.zip"
+  )
+
+  # If the data is downloaded from open government using `rgovcan`
+  govcan <- "da99526e-284f-4e06-8d04-193785cd1a96"
+
+  # Load
+  pipeload(urls = urls, govcan = govcan, output = glue("{path}raw/"), large = FALSE)
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # IMPORT DATA
+  # NOTE: optional
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  dat <- sf::st_read(
+    glue("{path}raw/CAN_DAS.gdb"),
+    layer = "DAS_Active_Inactive_Sites_2018",
+    quiet = TRUE
+  )
+  # _________________________________________________________________________________________ #
+
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # CREATE METADATA
+  # WARNING: mandatory
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  meta <- get_metadata(
+    pipeline_id = uid,
+    pipeline_crs = crs,
+    pipeline_bbox = bbox,
+    data_access = timestamp(),
+    data_bbox = sf::st_bbox(dat),
+    data_timespan = 2018
+  )
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # CREATE BIBTEX
+  # WARNING: mandatory
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  bib <- get_bib(uid)
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # APPLY SUBSETS AND CRS SPECIFIED BY USER
+  # NOTE: optional, only if applicable
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  dat <- dp_parameters(
+    dat,
+    crs = crs,
+    bbox = bbox
+  )
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # EXPORT
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Formatted data
+  fm <- glue("{path}/{nm}.geojson") # NOTE: not necessarily spatial data
+  sf::st_write(dat, dsn = fm, quiet = TRUE) # for spatial data
+
+  # Metadata
+  mt <- glue("{path}/{nm}.yaml")
+  yaml::write_yaml(meta, mt, column.major = FALSE)
+
+  # Bibtex
+  bi <- glue("{path}/{nm}.bib")
+  RefManageR::WriteBib(bib, file = bi, verbose = FALSE)
+  # _________________________________________________________________________________________ #
+}

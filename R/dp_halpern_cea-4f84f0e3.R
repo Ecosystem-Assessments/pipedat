1f9d2415efee6ed7c9e5fce5c4ca22ae2badf6f7
David Beauchesne <david.beauchesne@hotmail.com>
2022-05-31 16:39:30 -0400

    halpern grid integration pipeline (#22)

--- R/dp_halpern_cea-4f84f0e3.R
+++ R/dp_halpern_cea-4f84f0e3.R
@@ -28,84 +28,82 @@ dp_4f84f0e3 <- function(crs = 4326, bbox = NULL, timespan = NULL, halpern_years 
   # DOWNLOAD DATA
   # NOTE: optional
   # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
-  if (!exist$raw) {
-    # If the data is downloaded from online sources
-    urls <- data.frame(
-      urls = c(
-        # 2008
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_artisanal_fishing_mol_20150714093005",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_demersal_destructive_fishing_mol_20150714093045",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_demersal_nondest_high_bycatch_mol_20150714093145",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_demersal_nondest_low_bycatch_mol_20150714093246",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_inorganic_mol_20150714093318",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_invasives_mol_20150714093329",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_night_lights_mol_20150714092417",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_ocean_acidification_mol_20150714093342",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_ocean_pollution_mol_20150714093406",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_oil_rigs_lzw_mol_20150714092427",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_pelagic_high_bycatch_mol_20150714093423",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_pelagic_low_bycatch_mol_20150714093503",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_plumes_fert_mol_20150714092433",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_plumes_pest_mol_20150714092439",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_population_mol_20150714092443",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_shipping_mol_20150714093538",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_sst_mol_20150714092501",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_uv_mol_20150714092816",
-        # 2013
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_artisanal_fishing_mol_20150714095433",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_demersal_destructive_fishing_mol_20150714093559",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_demersal_nondest_high_bycatch_mol_20150714093620",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_demersal_nondest_low_bycatch_mol_20150714093643",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_inorganic_mol_20150714095441",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_invasives_mol_20150714093653",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_night_lights_mol_20150714093709",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_ocean_acidification_mol_20150714095453",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_ocean_pollution_mol_20150714093808",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_oil_rigs_lzw_mol_20150714093841",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_pelagic_high_bycatch_mol_20150714093845",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_pelagic_low_bycatch_mol_20150714093900",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_plumes_fert_mol_20150714093914",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_plumes_pest_mol_20150714093921",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_population_mol_20150714093926",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_shipping_mol_20150714094045",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_slr_mol_20150714094636",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_sst_mol_20150714094924",
-        "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_uv_mol_20150714095238"
-      ),
-      years = c(rep(2008, 18), rep(2013, 19)),
-      names = c(
-        # 2008
-        "artisanal_fishing", "demersal_destructive_fishing",
-        "demersal_nondest_high_bycatch", "demersal_nondest_low_bycatch", "inorganic",
-        "invasives", "night_lights", "ocean_acidification", "ocean_pollution", "oil_rigs",
-        "pelagic_high_bycatch", "pelagic_low_bycatch", "plumes_fert", "plumes_pest",
-        "population", "shipping", "sst", "uv",
-        # 2013
-        "artisanal_fishing", "demersal_destructive_fishing",
-        "demersal_nondest_high_bycatch", "demersal_nondest_low_bycatch", "inorganic",
-        "invasives", "night_lights", "ocean_acidification", "ocean_pollution", "oil_rigs",
-        "pelagic_high_bycatch", "pelagic_low_bycatch", "plumes_fert", "plumes_pest",
-        "population", "shipping", "slr", "sst", "uv"
-      )
+  urls <- data.frame(
+    urls = c(
+      # 2008
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_artisanal_fishing_mol_20150714093005",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_demersal_destructive_fishing_mol_20150714093045",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_demersal_nondest_high_bycatch_mol_20150714093145",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_demersal_nondest_low_bycatch_mol_20150714093246",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_inorganic_mol_20150714093318",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_invasives_mol_20150714093329",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_night_lights_mol_20150714092417",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_ocean_acidification_mol_20150714093342",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_ocean_pollution_mol_20150714093406",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_oil_rigs_lzw_mol_20150714092427",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_pelagic_high_bycatch_mol_20150714093423",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_pelagic_low_bycatch_mol_20150714093503",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_plumes_fert_mol_20150714092433",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_plumes_pest_mol_20150714092439",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_population_mol_20150714092443",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_shipping_mol_20150714093538",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_sst_mol_20150714092501",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_uv_mol_20150714092816",
+      # 2013
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_artisanal_fishing_mol_20150714095433",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2008_demersal_destructive_fishing_mol_20150714093559",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_demersal_nondest_high_bycatch_mol_20150714093620",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_demersal_nondest_low_bycatch_mol_20150714093643",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_inorganic_mol_20150714095441",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_invasives_mol_20150714093653",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_night_lights_mol_20150714093709",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_ocean_acidification_mol_20150714095453",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_ocean_pollution_mol_20150714093808",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_oil_rigs_lzw_mol_20150714093841",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_pelagic_high_bycatch_mol_20150714093845",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_pelagic_low_bycatch_mol_20150714093900",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_plumes_fert_mol_20150714093914",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_plumes_pest_mol_20150714093921",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_population_mol_20150714093926",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_shipping_mol_20150714094045",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_slr_mol_20150714094636",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_sst_mol_20150714094924",
+      "https://knb.ecoinformatics.org/knb/d1/mn/v2/object/raw_2013_uv_mol_20150714095238"
+    ),
+    years = c(rep(2008, 18), rep(2013, 19)),
+    names = c(
+      # 2008
+      "artisanal_fishing", "demersal_destructive_fishing",
+      "demersal_nondest_high_bycatch", "demersal_nondest_low_bycatch", "inorganic",
+      "invasives", "night_lights", "ocean_acidification", "ocean_pollution", "oil_rigs",
+      "pelagic_high_bycatch", "pelagic_low_bycatch", "plumes_fert", "plumes_pest",
+      "population", "shipping", "sst", "uv",
+      # 2013
+      "artisanal_fishing", "demersal_destructive_fishing",
+      "demersal_nondest_high_bycatch", "demersal_nondest_low_bycatch", "inorganic",
+      "invasives", "night_lights", "ocean_acidification", "ocean_pollution", "oil_rigs",
+      "pelagic_high_bycatch", "pelagic_low_bycatch", "plumes_fert", "plumes_pest",
+      "population", "shipping", "slr", "sst", "uv"
     )
+  )
 
-    # User can select which data they want
-    if (is.null(halpern_years)) {
-      halpern_years <- c(2008, 2013)
-    }
-    if (is.null(halpern_layers)) {
-      halpern_layers <- c(
-        "artisanal_fishing", "demersal_destructive_fishing", "demersal_destructive_fishing",
-        "demersal_nondest_high_bycatch", "demersal_nondest_low_bycatch", "inorganic",
-        "invasives", "night_lights", "ocean_acidification", "ocean_pollution", "oil_rigs",
-        "pelagic_high_bycatch", "pelagic_low_bycatch", "plumes_fert", "plumes_pest",
-        "population", "shipping", "slr", "sst", "uv"
-      )
-    }
+  # User can select which data they want
+  if (is.null(halpern_years)) {
+    halpern_years <- c(2008, 2013)
+  }
+  if (is.null(halpern_layers)) {
+    halpern_layers <- c(
+      "artisanal_fishing", "demersal_destructive_fishing", "demersal_destructive_fishing",
+      "demersal_nondest_high_bycatch", "demersal_nondest_low_bycatch", "inorganic",
+      "invasives", "night_lights", "ocean_acidification", "ocean_pollution", "oil_rigs",
+      "pelagic_high_bycatch", "pelagic_low_bycatch", "plumes_fert", "plumes_pest",
+      "population", "shipping", "slr", "sst", "uv"
+    )
+  }
+  iid <- urls$years %in% halpern_years & urls$names %in% halpern_layers
+  urls <- urls[iid, ]
 
-    iid <- urls$years %in% halpern_years & urls$names %in% halpern_layers
-    urls <- urls[iid, ]
-
+  if (!exist$raw) {
     # Load
     pipeload(
       urls = urls$urls,
@@ -119,7 +117,10 @@ dp_4f84f0e3 <- function(crs = 4326, bbox = NULL, timespan = NULL, halpern_years 
       files,
       function(x) file.rename(x, glue("{x}.zip"))
     )
+  }
+  # _________________________________________________________________________________________ #
 
+  if (!exist$clean) {
     # Unzip
     zipfiles <- basename(glue("{urls$urls}.zip"))
     if (2008 %in% halpern_years) {
@@ -142,10 +143,6 @@ dp_4f84f0e3 <- function(crs = 4326, bbox = NULL, timespan = NULL, halpern_years 
         )
       }
     }
-  }
-  # _________________________________________________________________________________________ #
-
-  if (!exist$clean) {
     # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
     # IMPORT DATA
     # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #

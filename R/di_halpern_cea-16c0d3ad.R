7b793a4305af8d8aa0dccc6508f6a8fb544c19ac
David Beauchesne <david.beauchesne@uqar.ca>
2022-05-31 16:38:59 -0400

    halpern grid integration pipeline

---
+++ R/di_halpern_cea-16c0d3ad.R
@@ -0,0 +1,98 @@
+#' @eval get_name("16c0d3ad")
+#'
+#' @eval get_description("16c0d3ad")
+#'
+#' @eval di_params()
+#'
+#' @family pipeline functions
+#' @rdname integration_pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: 16c0d3ad
+#'
+#' @examples
+#' \dontrun{
+#' di_16c0d3ad()
+#' }
+di_16c0d3ad <- function(grid = NULL, ...) {
+  # Output folders and other objects used
+  uid <- "16c0d3ad"
+  name <- get_shortname(uid)
+  nm <- glue("{name}-{uid}")
+  exist <- check_files(uid, name, ondisk = FALSE)
+  path <- make_output(uid, name)
+
+  if (!exist$integrated) {
+    # WARNING: For R CMD CHECK
+    grid_raster.tif <- intensity <- x <- y <- NULL
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # IMPORT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    data_id <- get_rawid(uid) # String with data to import
+    dat <- importdat(data_id)
+
+    # Study grid, if applicable
+    if (is.null(grid)) {
+      grid <- stars::read_stars("data/data-grid/grid_raster.tif", quiet = TRUE)
+    }
+    grid <- sf::st_transform(grid, st_crs(dat[[1]]))
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # ANALYZE / FORMAT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    name <- names(dat)
+    name <- gsub("halpern_cea-4f84f0e3-", "", name)
+    name <- gsub(".tif", "", name)
+    pipedat_rasterize <- function(dat, name) {
+      dat <- stars::st_warp(dat, grid) |>
+        c(grid) |>
+        as.data.frame() |>
+        dplyr::filter(!is.na(grid_raster.tif)) |>
+        dplyr::arrange(uid) |>
+        dplyr::select(-x, -y) |>
+        stats::setNames(c("intensity", "uid")) |>
+        dplyr::filter(intensity > 0) |>
+        dplyr::select(uid, intensity)
+      colnames(dat)[2] <- name
+      dat
+    }
+    for (i in 1:length(dat)) dat[[i]] <- pipedat_rasterize(dat[[i]], name[i])
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE METADATA
+    # WARNING: mandatory
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    meta <- get_metadata(
+      pipeline_type = "integration",
+      pipeline_id = uid,
+      integration_data = data_id,
+      integration_grid = get_grid_info(grid) # if applicable
+    )
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE BIBTEX
+    # WARNING: mandatory
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    bib <- get_bib(uid)
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # EXPORT
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Formatted data
+    fm <- here::here(path, glue("{nm}-{name}.csv"))
+    for (i in 1:length(fm)) utils::write.csv(dat[[i]], fm[i], row.names = FALSE)
+
+    # Metadata
+    mt <- here::here(path, glue("{nm}.yaml"))
+    yaml::write_yaml(meta, mt, column.major = FALSE)
+
+    # Bibtex
+    bi <- here::here(path, glue("{nm}.bib"))
+    RefManageR::WriteBib(bib, file = bi, verbose = FALSE)
+    # _________________________________________________________________________________________ #
+  }
+}

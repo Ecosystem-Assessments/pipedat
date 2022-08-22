666473a64acf66d7a693d25dcf91f779df92d379
David Beauchesne <david.beauchesne@uqar.ca>
2022-08-22 15:03:01 -0400

    start of species list pipeline, to complete

---
+++ R/di_species_list_nw_atlantic-893b37e8.R
@@ -0,0 +1,118 @@
+#' @eval get_name("893b37e8")
+#'
+#' @eval get_description("893b37e8")
+#'
+#' @eval dp_params()
+#' @eval di_params()
+#' @param ... further arguments used in functions, if applicable.
+#'
+#' @family pipeline functions
+#' @rdname integration_pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: 893b37e8
+#'
+#' @examples
+#' \dontrun{
+#' di_893b37e8()
+#' }
+di_893b37e8 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, grid = NULL, ...) {
+  # Output folders and other objects used
+  uid <- "893b37e8"
+  name <- get_shortname(uid)
+  nm <- glue("{name}-{uid}")
+  exist <- check_files(uid, name, ondisk = FALSE)
+  path <- make_output(uid, name)
+
+  if (!exist$integrated) {
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # IMPORT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    raw_id <- get_rawid(uid) # String with data to import
+    pipedat(raw_id, bbox, bbox_crs, timespan)
+    dat <- importdat(raw_id) 
+    carms <- dat["carms_checklist-084860fd.csv"][[1]]
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # ANALYZE / FORMAT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    obs_lists <- dat[
+      c(
+        "dfo_survey_4vsw-2aafec74-gscat.csv",
+        "dfo_survey_fall-90e90110-gscat.csv",
+        "dfo_survey_spring-21f8a758-gscat.csv",
+        "dfo_survey_summer-3348d162-gscat.csv"
+      )
+    ]
+    sp_lists <- dat[
+      c(
+        "dfo_survey_4vsw-2aafec74-gsspecies.csv",
+        "dfo_survey_fall-90e90110-gsspecies.csv",
+        "dfo_survey_spring-21f8a758-gsspecies.csv",
+        "dfo_survey_summer-3348d162-gsspecies.csv"        
+      )
+    ]
+    
+  
+    for(i in 1:length(obs_lists)) {
+      spfq <- table(obs_lists[[i]]$SPEC) |> 
+              as.data.frame() |>
+              dplyr::mutate(Var1 = as.integer(as.character(Var1)))
+      spid <- unique(obs_lists[[i]]$SPEC)
+      lsid <- sp_lists[[i]]$CODE %in% spid
+      sp_lists[[i]] <- sp_lists[[i]][lsid, ] |>
+                       dplyr::left_join(spfq, by = c("CODE" = "Var1"))
+    }
+    
+    # -----
+    species <- dplyr::bind_rows(sp_lists) |>
+               dplyr::group_by(CODE, SPEC, COMM) |>
+               dplyr::summarise(Freq = sum(Freq))
+               
+    # Adjust species scientific names
+    species$SPEC <- stringr::str_to_sentence(species$SPEC)
+
+    # Check with CaRMS
+    species$SPEC %in% carms$ScientificName
+    
+    "dfo_survey_4vsw-2aafec74-gscat.csv"
+    
+    "dfo_survey_4vsw-2aafec74-gsspecies.csv"
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE METADATA
+    # WARNING: mandatory
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    meta <- get_metadata(
+      pipeline_type = "integration",
+      pipeline_id = uid,
+      integration_data = raw_id
+    )
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE BIBTEX
+    # WARNING: mandatory
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    bib <- get_bib(uid)
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # EXPORT 
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Formatted data 
+    fm <- here::here(path,glue("{nm}.csv"))
+    utils::write.csv(dat, fm, row.names = FALSE)
+
+    # Metadata
+    mt <- here::here(path,glue("{nm}.yaml"))
+    yaml::write_yaml(meta, mt, column.major = FALSE)
+    
+    # Bibtex
+    bi <- here::here(path,glue("{nm}.bib"))
+    RefManageR::WriteBib(bib, file = bi, verbose = FALSE)
+    # _________________________________________________________________________________________ #
+  }
+}

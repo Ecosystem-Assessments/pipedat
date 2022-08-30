ee04ed98d2b77523b1560e4d8ab21b56d347508d
David Beauchesne <david.beauchesne@uqar.ca>
2022-08-30 08:58:46 -0400

    add venter data

--- R/dp_terrestrial_human_footprint_venter-103a233e.R
+++ R/dp_terrestrial_human_footprint_venter-103a233e.R
@@ -1,6 +1,6 @@
-#' @eval get_name("e2349037")
+#' @eval get_name("103a233e")
 #'
-#' @eval get_description("e2349037")
+#' @eval get_description("103a233e")
 #'
 #' @eval dp_params()
 #'
@@ -8,15 +8,15 @@
 #' @rdname data_pipelines
 #' @seealso \code{\link{pipedat}}
 #'
-#' @keywords pipeline_id: e2349037
+#' @keywords pipeline_id: 103a233e
 #'
 #' @examples
 #' \dontrun{
-#' dp_e2349037()
+#' dp_103a233e()
 #' }
-dp_e2349037 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ...) {
+dp_103a233e <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ...) {
   # Output folders and other objects used
-  uid <- "e2349037"
+  uid <- "103a233e"
   name <- get_shortname(uid)
   nm <- glue("{name}-{uid}")
   exist <- check_files(uid, name, ondisk = FALSE)
@@ -27,41 +27,16 @@ dp_e2349037 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ...) {
   # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
   if (!exist$raw) {
     # If the data is downloaded from online sources
-    urls <- c(
-      "https://datadryad.org/stash/downloads/file_stream/402761",
-      "https://datadryad.org/stash/downloads/file_stream/402868",
-      "https://datadryad.org/stash/downloads/file_stream/402897",
-      "https://datadryad.org/stash/downloads/file_stream/402762",
-      "https://datadryad.org/stash/downloads/file_stream/402763"
-    )
-
-    # Load
+    urls <- "https://datadryad.org/stash/downloads/file_stream/100086"
     pipeload(
       urls = urls,
       output = here::here(path, "raw"),
       large = TRUE
     )
-
     # Rename files
     file.rename(
-      here::here(path, "raw", "402761"),
-      here::here(path, "raw", "human_footprint_grass_scripts-402761.zip")
-    )
-    file.rename(
-      here::here(path, "raw", "402868"),
-      here::here(path, "raw", "human_footprint_maps-402868.zip")
-    )
-    file.rename(
-      here::here(path, "raw", "402897"),
-      here::here(path, "raw", "human_footprint_pressure_layers-402897.zip")
-    )
-    file.rename(
-      here::here(path, "raw", "402762"),
-      here::here(path, "raw", "human_footprint_reclassification_files-402762.zip")
-    )
-    file.rename(
-      here::here(path, "raw", "402763"),
-      here::here(path, "raw", "williams_et_al_results-402763.xls")
+      here::here(path, "raw", "100086"),
+      here::here(path, "raw", "HumanFootprintv2.7z")
     )
   }
   # _________________________________________________________________________________________ #
@@ -70,17 +45,17 @@ dp_e2349037 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ...) {
     # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
     # IMPORT DATA
     # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
-    # Unzip
-    zipfiles <- dir(here::here(path, "raw"), pattern = ".zip", full.names = TRUE)
-    lapply(zipfiles, function(x) utils::unzip(x, exdir = here::here(path, "raw")))
+    archive::archive_extract(
+      here::here(path, "raw", "HumanFootprintv2.7z"),
+      dir = here::here(path, "raw", "HumanFootprint")
+    )
 
-    files <- dir(here::here(path, "raw", "Human_footprint_pressure_layers"), full.names = TRUE)
-    pressures <- lapply(files, masterload)
-
-    files <- dir(here::here(path, "raw", "Human_footprint_maps"), full.names = TRUE)
-    footprint <- lapply(files, masterload)
-
-    dat <- c(pressures, footprint)
+    # Load all
+    files <- dir(here::here(path, "raw", "HumanFootprint", "Dryadv3", "Maps"), pattern = ".tif$")
+    dat <- lapply(
+      here::here(path, "raw", "HumanFootprint", "Dryadv3", "Maps", files),
+      masterload
+    )
     datnames <- lapply(dat, names) |>
       unlist() |>
       tools::file_path_sans_ext()
@@ -121,10 +96,7 @@ dp_e2349037 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ...) {
     }
 
     # Delete to save memory
-    unlink(here::here(path, "raw", "Human_footprint_grass_scripts"), recursive = TRUE)
-    unlink(here::here(path, "raw", "Human_footprint_maps"), recursive = TRUE)
-    unlink(here::here(path, "raw", "Human_footprint_pressure_layers"), recursive = TRUE)
-    unlink(here::here(path, "raw", "human_footprint_reclassification_files"), recursive = TRUE)
+    unlink(here::here(path, "raw", "HumanFootprint"), recursive = TRUE)
 
     # Metadata
     mt <- here::here(path, glue("{nm}.yaml"))

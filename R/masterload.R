9412d9981f3e2df94e670514d62eb3d21db436cc
David Beauchesne <david.beauchesne@uqar.ca>
2022-09-01 10:58:10 -0400

    new template for govcan data

--- R/masterload.R
+++ R/masterload.R
@@ -1,12 +1,13 @@
-#' Imports data in global R environment
+#' Imports and export data in or from global R environment
 #'
-#' This function is used to import data of various format in R
+#' These functions are used to import or export data of various format in R
 #'
-#' @param path path to disk of the file to import
+#' @param path path to disk of the file to import or export. Do not specify the extension if exporting using `masterwrite()`.
+#' @param obj object to write to disk
 #'
 #' @return This function returns objects in the global R environment
 #'
-#' @export
+#' @name masterload
 #'
 #' @examples
 #' \dontrun{
@@ -39,4 +40,48 @@ masterload <- function(path) {
 
   # -----
   invisible(dat)
+}
+
+#' @name masterload
+#' @export
+masterwrite <- function(obj, path) {
+  # Identify class of object
+  cls <- class(obj)
+
+  # Import file depending on extension
+  ## GEOJSON
+  if ("sf" %in% cls) {
+    ext <- "geojson"
+    sf::st_write(obj, glue("{path}.{ext}"), quiet = TRUE)
+  }
+
+  ## GeoTIFF
+  if ("stars" %in% cls) {
+    ext <- "tif"
+    stars::write_stars(obj, glue("{path}.{ext}"), quiet = TRUE)
+  }
+
+  ## CSV
+  if (
+    ("matrix" %in% cls | "data.frame" %in% cls) &
+      (!"stars" %in% cls & !"sf" %in% cls)
+  ) {
+    ext <- "csv"
+    utils::write.csv(obj, glue("{path}.{ext}"), row.names = FALSE)
+  }
+
+  ## YAML
+  if ("list" %in% cls) {
+    ext <- "yaml"
+    yaml::write_yaml(obj, glue("{path}.{ext}"), column.major = FALSE)
+  }
+
+  ## Bibtex
+  if ("BibEntry" %in% cls) {
+    ext <- "bib"
+    RefManageR::WriteBib(obj, file = glue("{path}.{ext}"), verbose = FALSE)
+  }
+
+  # Identify which data were loaded
+  msgInfo("Exported file:", glue("{basename(path)}.{ext}"))
 }

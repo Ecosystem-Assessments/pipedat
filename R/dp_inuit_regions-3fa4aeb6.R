625f558b973a07e5633b998066d322f0b0df5a97
David Beauchesne <david.beauchesne@uqar.ca>
2022-09-02 12:10:21 -0400

    inuit regions

---
+++ R/dp_inuit_regions-3fa4aeb6.R
@@ -0,0 +1,87 @@
+#' @eval get_name("3fa4aeb6")
+#'
+#' @eval get_description("3fa4aeb6")
+#'
+#' @eval dp_params()
+#'
+#' @family pipeline functions
+#' @rdname data_pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: 3fa4aeb6
+#'
+#' @examples
+#' \dontrun{
+#' dp_3fa4aeb6()
+#' }
+dp_3fa4aeb6 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ...) {
+  # Output folders and other objects used
+  uid <- "3fa4aeb6"
+  name <- get_shortname(uid)
+  nm <- glue("{name}-{uid}")
+  exist <- check_files(uid, name, ondisk = FALSE)
+  path <- make_output(uid, name)
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # DOWNLOAD DATA
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  if (!exist$raw) {
+    govcan <- get_pipeline(uid)$data_uuid
+    pipeload(
+      govcan = govcan,
+      output = here::here(path, "raw"),
+      large = FALSE
+    )
+  }
+  # _________________________________________________________________________________________ #
+
+  if (!exist$clean) {
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # IMPORT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    dat <- masterload(here::here(path, "raw", "Inuit_Region_Region_inuite.shp"))
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE METADATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    meta <- get_metadata(
+      pipeline_type = "data",
+      pipeline_id = uid,
+      pipeline_bbox = bbox,
+      pipeline_bbox_crs = bbox_crs,
+      access = timestamp(),
+      data_bbox = sf::st_bbox(dat)
+    )
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE BIBTEX
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    bib <- get_bib(uid)
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # APPLY SUBSETS SPECIFIED BY USER
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    dat <- dp_parameters(
+      dat,
+      bbox = bbox,
+      bbox_crs = bbox_crs
+    )
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # EXPORT
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Formatted data
+    fm <- here::here(path, glue("{nm}"))
+    masterwrite(dat, fm)
+
+    # Metadata & bibtex
+    mt <- here::here(path, nm)
+    masterwrite(meta, mt)
+    masterwrite(bib, mt)
+    # _________________________________________________________________________________________ #
+  } # if exist clean, don't run again
+}

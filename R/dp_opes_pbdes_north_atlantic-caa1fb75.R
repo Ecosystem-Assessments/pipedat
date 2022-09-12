8de05d58fc8d80f996928901eaba5e22f694b17d
David Beauchesne <david.beauchesne@uqar.ca>
2022-09-12 11:57:54 -0400

    add contaminants

---
+++ R/dp_opes_pbdes_north_atlantic-caa1fb75.R
@@ -0,0 +1,97 @@
+#' @eval get_name("caa1fb75")
+#'
+#' @eval get_description("caa1fb75")
+#'
+#' @eval dp_params()
+#'
+#' @family pipeline functions
+#' @rdname data_pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: caa1fb75
+#'
+#' @examples
+#' \dontrun{
+#' dp_caa1fb75()
+#' }
+dp_caa1fb75 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ...) {
+  # Output folders and other objects used
+  uid <- "caa1fb75"
+  name <- get_shortname(uid)
+  nm <- glue("{name}-{uid}")
+  exist <- check_files(uid, name, ondisk = FALSE)
+  path <- make_output(uid, name)
+    
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # DOWNLOAD DATA
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  if (!exist$raw) {
+    govcan <- get_pipeline(uid)$data_uuid
+    pipeload(
+      govcan = govcan, 
+      output = here::here(path, "raw"), 
+      large = FALSE
+    )
+  }
+  # _________________________________________________________________________________________ #
+    
+  if (!exist$clean) {
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # IMPORT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    p <- here::here(path, "raw", "NCP_NorthAtlantic_OPEs_PBDEs_Concentrations_EN_FR.csv")
+    ttl <- readLines(p)[34] |> stringr::str_split(pattern = ",")
+    dat <- readLines(p)[35:43] |> 
+           stringr::str_split(pattern = ",") |>
+           lapply(setNames, nm = ttl[[1]]) |>
+           dplyr::bind_rows()  |> 
+           sf::st_as_sf(coords = c("Longitude","Latitude"), crs = 4326, remove = FALSE)    
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE METADATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    meta <- get_metadata(
+      pipeline_type = "data",
+      pipeline_id = uid,
+      pipeline_bbox = bbox, 
+      pipeline_bbox_crs = bbox_crs, 
+      access = timestamp(), 
+      data_bbox = sf::st_bbox(dat), 
+      data_timespan = 2015
+    )
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE BIBTEX
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    bib <- get_bib(uid)
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # APPLY SUBSETS SPECIFIED BY USER
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # on.exit(sf::sf_use_s2(TRUE), add = TRUE)
+    # sf::sf_use_s2(FALSE)
+    # dat <- lapply(dat, dp_parameters, bbox = bbox, bbox_crs = bbox_crs, timespan = timespan)
+    dat <- dp_parameters(
+      dat,
+      bbox = bbox,
+      bbox_crs = bbox_crs
+    )
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # EXPORT 
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Formatted data   
+    fm <- here::here(path,glue("{nm}"))
+    masterwrite(dat, fm)
+    
+    # Metadata & bibtex
+    mt <- here::here(path, nm)
+    masterwrite(meta, mt)
+    masterwrite(bib, mt)  
+    # _________________________________________________________________________________________ #
+  } #if exist clean, don't run again
+}

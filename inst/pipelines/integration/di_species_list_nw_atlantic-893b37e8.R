5de7d29d14f40a315ad9135f80804523929283ee
David Beauchesne <david.beauchesne@uqar.ca>
2022-10-31 14:24:08 -0400

    adjust code, extra functions in pipelines must be at beginning of function

--- inst/pipelines/integration/di_species_list_nw_atlantic-893b37e8.R
+++ inst/pipelines/integration/di_species_list_nw_atlantic-893b37e8.R
@@ -17,6 +17,130 @@
 #' di_893b37e8()
 #' }
 di_893b37e8 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, grid = NULL, ...) {
+  # CODE to remove, specific to Atlantic surveys
+  remove_code <- function(df) {
+    remco <- c(
+      1510, # Buccinidae eggs (259)
+      2529, # Calappa megalops (1)
+      1530, # Cephalopoda eggs (4)
+      290, # Clupeidae/osmeridae (1)
+      2507, # Crab (13)
+      2001, # Crustacea larvae (1)
+      8000, # Ctenophores,coelenterates,porifera (8)
+      1100, # Eggs (17)
+      100, # Finfishes (6)
+      1200, # Fish eggs-unidentified (11)
+      9400, # Foreign articles,garbage (10)
+      9301, # Fucus (13)
+      1511, # Gasteropoda eggs (12)
+      8363, # Halipterus (4)
+      1218, # Hemitripterus americanus, eggs (1)
+      2810, # Hyperia (2)
+      2802, # Hyperia oculata (1)
+      1312, # Hyppolytid eggs (1)
+      1600, # Invertebrate eggs (3)
+      2525, # Lithodes/neolitodes (31)
+      4514, # Loliginidae,ommastrephidae (55)
+      1701, # Marine invertebrata (1)
+      1500, # Mollusca eggs (8)
+      1228, # Myoxocephalus eggs (1)
+      4233, # Nassa bivittata (3)
+      4235, # Nassariidae thaisidae (1)
+      8366, # No longer used - phakellia (2)
+      8355, # Octopoda, cirrata (3)
+      9630, # Organic debris (39)
+      7000, # Parasites,round worms (1)
+      4349, # Pectinidae shells (4)
+      4310, # Protobranchia, heterodonta (70)
+      1199, # Purse barndoor skate (2)
+      1203, # Purse little skate (37)
+      1202, # Purse smooth skate (5)
+      1201, # Purse thorny skate (8)
+      1204, # Purse winter skate (24)
+      1224, # Raja eggs (462)
+      9999, # Reserved (14)
+      3999, # Sand tube (2)
+      8530, # Sea corals (24)
+      592, # Shark (1)
+      2499, # Shrimp-like (1)
+      8327, # Coral unidentified (130)
+      9200, # Stones and rocks (27)
+      9300, # Thallophyta (49)
+      9003, # Unid fish and eggs (1)
+      9001, # Unid fish and invertebrates (3)
+      9002, # Unid fish and remains (1)
+      9000, # Unid remains,digested (26)
+      90, # Fish (13)
+      9991, # Unidentified (341)
+      9992, # Unidentified (132)
+      9993, # Unidentified (89)
+      9994, # Unidentified (68)
+      9995, # Unidentified (55)
+      9996, # Unidentified (42)
+      9997, # Unidentified (29)
+      9998, # Unidentified (25)
+      1091, # Unidentified a (78)
+      1092, # Unidentified b (20)
+      1093, # Unidentified c (10)
+      1094, # Unidentified d (10)
+      1095, # Unidentified e (6)
+      979, # Alepocephalus (1) - Kept at the species level
+      928, # Argyropelecus (2) - Kept at the species level
+      295, # Bathylagus (1) - Kept at the species level
+      1007, # Bathyraja (1) - Kept at the species level
+      515, # Careproctus (2) - Kept at the species level
+      1064, # Coryphaenoides (1) - Kept at the species level
+      4569, # Gonatus (4) - Kept at the species level
+      1191, # Leucoraja (482) - Kept at the species level
+      1013, # Nezumia (5) - Kept at the species level
+      565, # Paralepis (5) - Kept at the species level
+      1052 # Rouleina (1) - Kept at the species level
+    )
+
+    iid <- df$CODE %in% remco
+    df[!iid, ]
+  }
+
+
+  # Change names
+  change_taxa_name <- function(df) {
+    dat <- rbind(
+      c("Henrica", "Henricia"),
+      c("Ophiura sarsi", "Ophiura sarsii"),
+      c("Acanthephyra exemia", "Acanthephyra eximia"),
+      c("Aristaepsis edwardsinana", "Aristaeopsis edwardsiana"),
+      c("Arrhis phyllonix", "Arrhis phyllonyx"),
+      c("Arrhoges occidentali", "Arrhoges occidentalis"),
+      c("Astrotecten duplicatus", "Astropecten duplicatus"),
+      c("Calathura branchiata", "Calathura brachiata"),
+      c("Ceremaster granularis", "Ceramaster granularis"),
+      c("Chionoecetes opili", "Chionoecetes opilio"),
+      c("Evermanella indica", "Evermannella indica"),
+      c("Glycera", "Ichnopus"),
+      c("Gonatus steenstrupii", "Gonatus steenstrupi"),
+      c("Gorgonocephalidae,asteronychidae", "Gorgonocephalus"),
+      c("Gorgonocephalus lamarcki", "Gorgonocephalus lamarckii"),
+      c("Margarites groenlandica", "Margarites groenlandicus"),
+      c("Omosudis lowei", "Omosudis lowii"),
+      c("Paralepis atlantica kroyer", "Magnisudis atlantica"),
+      c("Pitar morrhuana", "Agriopoma morrhuanum"),
+      c("Porania pulvilis", "Porania pulvillus"),
+      c("Poraniomorpha borealis", "Poraniomorpha hispida"),
+      c("Sabinea sarsi", "Sabinea sarsii"),
+      c("Sergestes", "Eusergestes arcticus"),
+      c("Sergestes arcticus", "Eusergestes arcticus"),
+      c("Serrivomer beani", "Serrivomer beanii"),
+      c("Terebratulina septentrionali", "Terebratulina septentrionalis"),
+      c("Velutina laevigata", "Velutina velutina")
+    )
+    dat <- data.frame(from = dat[, 1], to = dat[, 2])
+    for (i in 1:nrow(dat)) {
+      iid <- df$SPEC %in% dat$from[i]
+      df$SPEC[iid] <- dat$to[i]
+    }
+    df
+  }
+  
   # Output folders and other objects used
   uid <- "893b37e8"
   nm <- glue::glue("{get_shortname(uid)}-{uid}")
@@ -112,128 +236,4 @@ di_893b37e8 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, grid = NU
     RefManageR::WriteBib(bib, file = bi, verbose = FALSE)
     # _________________________________________________________________________________________ #
   }
-}
-
-# CODE to remove, specific to Atlantic surveys
-remove_code <- function(df) {
-  remco <- c(
-    1510, # Buccinidae eggs (259)
-    2529, # Calappa megalops (1)
-    1530, # Cephalopoda eggs (4)
-    290, # Clupeidae/osmeridae (1)
-    2507, # Crab (13)
-    2001, # Crustacea larvae (1)
-    8000, # Ctenophores,coelenterates,porifera (8)
-    1100, # Eggs (17)
-    100, # Finfishes (6)
-    1200, # Fish eggs-unidentified (11)
-    9400, # Foreign articles,garbage (10)
-    9301, # Fucus (13)
-    1511, # Gasteropoda eggs (12)
-    8363, # Halipterus (4)
-    1218, # Hemitripterus americanus, eggs (1)
-    2810, # Hyperia (2)
-    2802, # Hyperia oculata (1)
-    1312, # Hyppolytid eggs (1)
-    1600, # Invertebrate eggs (3)
-    2525, # Lithodes/neolitodes (31)
-    4514, # Loliginidae,ommastrephidae (55)
-    1701, # Marine invertebrata (1)
-    1500, # Mollusca eggs (8)
-    1228, # Myoxocephalus eggs (1)
-    4233, # Nassa bivittata (3)
-    4235, # Nassariidae thaisidae (1)
-    8366, # No longer used - phakellia (2)
-    8355, # Octopoda, cirrata (3)
-    9630, # Organic debris (39)
-    7000, # Parasites,round worms (1)
-    4349, # Pectinidae shells (4)
-    4310, # Protobranchia, heterodonta (70)
-    1199, # Purse barndoor skate (2)
-    1203, # Purse little skate (37)
-    1202, # Purse smooth skate (5)
-    1201, # Purse thorny skate (8)
-    1204, # Purse winter skate (24)
-    1224, # Raja eggs (462)
-    9999, # Reserved (14)
-    3999, # Sand tube (2)
-    8530, # Sea corals (24)
-    592, # Shark (1)
-    2499, # Shrimp-like (1)
-    8327, # Coral unidentified (130)
-    9200, # Stones and rocks (27)
-    9300, # Thallophyta (49)
-    9003, # Unid fish and eggs (1)
-    9001, # Unid fish and invertebrates (3)
-    9002, # Unid fish and remains (1)
-    9000, # Unid remains,digested (26)
-    90, # Fish (13)
-    9991, # Unidentified (341)
-    9992, # Unidentified (132)
-    9993, # Unidentified (89)
-    9994, # Unidentified (68)
-    9995, # Unidentified (55)
-    9996, # Unidentified (42)
-    9997, # Unidentified (29)
-    9998, # Unidentified (25)
-    1091, # Unidentified a (78)
-    1092, # Unidentified b (20)
-    1093, # Unidentified c (10)
-    1094, # Unidentified d (10)
-    1095, # Unidentified e (6)
-    979, # Alepocephalus (1) - Kept at the species level
-    928, # Argyropelecus (2) - Kept at the species level
-    295, # Bathylagus (1) - Kept at the species level
-    1007, # Bathyraja (1) - Kept at the species level
-    515, # Careproctus (2) - Kept at the species level
-    1064, # Coryphaenoides (1) - Kept at the species level
-    4569, # Gonatus (4) - Kept at the species level
-    1191, # Leucoraja (482) - Kept at the species level
-    1013, # Nezumia (5) - Kept at the species level
-    565, # Paralepis (5) - Kept at the species level
-    1052 # Rouleina (1) - Kept at the species level
-  )
-
-  iid <- df$CODE %in% remco
-  df[!iid, ]
-}
-
-
-# Change names
-change_taxa_name <- function(df) {
-  dat <- rbind(
-    c("Henrica", "Henricia"),
-    c("Ophiura sarsi", "Ophiura sarsii"),
-    c("Acanthephyra exemia", "Acanthephyra eximia"),
-    c("Aristaepsis edwardsinana", "Aristaeopsis edwardsiana"),
-    c("Arrhis phyllonix", "Arrhis phyllonyx"),
-    c("Arrhoges occidentali", "Arrhoges occidentalis"),
-    c("Astrotecten duplicatus", "Astropecten duplicatus"),
-    c("Calathura branchiata", "Calathura brachiata"),
-    c("Ceremaster granularis", "Ceramaster granularis"),
-    c("Chionoecetes opili", "Chionoecetes opilio"),
-    c("Evermanella indica", "Evermannella indica"),
-    c("Glycera", "Ichnopus"),
-    c("Gonatus steenstrupii", "Gonatus steenstrupi"),
-    c("Gorgonocephalidae,asteronychidae", "Gorgonocephalus"),
-    c("Gorgonocephalus lamarcki", "Gorgonocephalus lamarckii"),
-    c("Margarites groenlandica", "Margarites groenlandicus"),
-    c("Omosudis lowei", "Omosudis lowii"),
-    c("Paralepis atlantica kroyer", "Magnisudis atlantica"),
-    c("Pitar morrhuana", "Agriopoma morrhuanum"),
-    c("Porania pulvilis", "Porania pulvillus"),
-    c("Poraniomorpha borealis", "Poraniomorpha hispida"),
-    c("Sabinea sarsi", "Sabinea sarsii"),
-    c("Sergestes", "Eusergestes arcticus"),
-    c("Sergestes arcticus", "Eusergestes arcticus"),
-    c("Serrivomer beani", "Serrivomer beanii"),
-    c("Terebratulina septentrionali", "Terebratulina septentrionalis"),
-    c("Velutina laevigata", "Velutina velutina")
-  )
-  dat <- data.frame(from = dat[, 1], to = dat[, 2])
-  for (i in 1:nrow(dat)) {
-    iid <- df$SPEC %in% dat$from[i]
-    df$SPEC[iid] <- dat$to[i]
-  }
-  df
 }

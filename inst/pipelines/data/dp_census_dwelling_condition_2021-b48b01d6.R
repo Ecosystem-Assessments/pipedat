19cba624451b9859cfd9d6fcec5b5101e8d2dd14
David Beauchesne <david.beauchesne@uqar.ca>
2022-11-10 11:31:43 -0500

    housing census stats pipelines

---
+++ inst/pipelines/data/dp_census_dwelling_condition_2021-b48b01d6.R
@@ -0,0 +1,76 @@
+#' @eval get_name("b48b01d6")
+#'
+#' @eval get_description("b48b01d6")
+#'
+#' @eval dp_params()
+#'
+#' @family pipeline functions
+#' @rdname data_pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: b48b01d6
+#'
+#' @examples
+#' \dontrun{
+#' dp_b48b01d6()
+#' }
+dp_b48b01d6 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ...) {
+  # Output folders and other objects used
+  uid <- "b48b01d6"
+  nm <- glue::glue("{get_shortname(uid)}-{uid}")
+  exist <- check_files(uid)
+  path <- make_output(uid)
+
+    
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # DOWNLOAD DATA
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  if (!exist$raw) {
+    # If the data is downloaded from online sources
+    urls <- "https://www150.statcan.gc.ca/n1/tbl/csv/98100233-eng.zip"
+    pipeload(urls = urls, output = here::here(path, "raw"), large = TRUE)
+  }
+  # _________________________________________________________________________________________ #
+    
+  if (!exist$clean) {
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # IMPORT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    utils::unzip(here::here(path, "raw", "98100233-eng.zip"), exdir = here::here(path, "raw"))
+    dat <- vroom::vroom(here::here(path,"raw","98100233.csv"))
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE METADATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    meta <- get_metadata(
+      pipeline_type = "data",
+      pipeline_id = uid,
+      access = timestamp(), 
+      data_timespan = 2021
+    )
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE BIBTEX
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    bib <- get_bib(uid)
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # EXPORT 
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Formatted data   
+    fm <- here::here(path,glue::glue("{nm}.csv"))
+    vroom::vroom_write(dat, glue::glue("{fm}.csv"), delim = ",")
+
+    # Delete to save memory
+    unlink(here::here(path, "raw", "98100233.csv"), recursive = TRUE)
+
+    # Metadata & bibtex
+    mt <- here::here(path, nm)
+    masterwrite(meta, mt)
+    masterwrite(bib, mt)  
+    # _________________________________________________________________________________________ #
+  } #if exist clean, don't run again
+}

e9276a8b3e2758b4c6431533abfd6ee4406fa02f
David Beauchesne <david.beauchesne@uqar.ca>
2022-11-10 05:36:19 -0500

    npri data

---
+++ inst/pipelines/data/dp_national_pollutant_release_inventory-d2f44fdf.R
@@ -0,0 +1,120 @@
+#' @eval get_name("d2f44fdf")
+#'
+#' @eval get_description("d2f44fdf")
+#'
+#' @eval dp_params()
+#'
+#' @family pipeline functions
+#' @rdname data_pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: d2f44fdf
+#'
+#' @examples
+#' \dontrun{
+#' dp_d2f44fdf()
+#' }
+dp_d2f44fdf <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ...) {
+  # Output folders and other objects used
+  uid <- "d2f44fdf"
+  nm <- glue::glue("{get_shortname(uid)}-{uid}")
+  exist <- check_files(uid)
+  path <- make_output(uid)
+
+    
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # DOWNLOAD DATA
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  if (!exist$raw) {
+    govcan <- get_pipeline(uid)$data_uuid
+    pipeload(
+      govcan = govcan, 
+      output = here::here(path, "raw"), 
+      large = FALSE
+    )
+  }
+  # _________________________________________________________________________________________ #
+    
+  if (!exist$clean) {
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # IMPORT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # There are more data files than that. Edit script if I want more at some point.
+    files <- c(
+      # "NPRI-INRP_CommentsCommentaires_1997-present.csv",
+      # "NPRI-INRP_DisposalsEliminations_1993-present.csv",
+      # "NPRI-INRP_DisposalsEliminations_TransfersTransferts_1993-present.csv",
+      "NPRI-INRP_GeolocationsGeolocalisation_1993-present.csv"#,
+      # "NPRI-INRP_ReleasesRejets_1993-present.csv"
+    )
+    filepaths <- here::here(path,"raw",files)
+    # dat <- lapply(filepaths, masterload)
+    dat <- masterload(filepaths)
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # FORMAT DATA
+    # WARNING: In order for filters to work, names of column should be: 
+    #             year      = year
+    #             longitude = longitude
+    #             latitude  = latitude
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    dat <- dplyr::rename(
+      dat,
+      longitude = Longitude, 
+      latitude = Latitude,
+      year = ReportYear
+    ) |>
+    dplyr::filter(!is.na(longitude)) |>
+    sf::st_as_sf(coords = c("longitude","latitude"), crs = 4326)
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE METADATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    meta <- get_metadata(
+      pipeline_type = "data",
+      pipeline_id = uid,
+      pipeline_bbox = bbox, 
+      pipeline_bbox_crs = bbox_crs, 
+      pipeline_timespan = timespan, 
+      access = timestamp(), 
+      data_bbox = sf::st_bbox(dat), 
+      data_timespan = sort(unique(dat$year))
+    )
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE BIBTEX
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    bib <- get_bib(uid)
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # APPLY SUBSETS SPECIFIED BY USER
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # on.exit(sf::sf_use_s2(TRUE), add = TRUE)
+    # sf::sf_use_s2(FALSE)
+    # dat <- lapply(dat, dp_parameters, bbox = bbox, bbox_crs = bbox_crs, timespan = timespan)
+    dat <- dp_parameters(
+      dat,
+      bbox = bbox,
+      bbox_crs = bbox_crs,
+      timespan = timespan
+    )
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # EXPORT 
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Formatted data   
+    fm <- here::here(path,glue::glue("{nm}"))
+    masterwrite(dat, fm)
+    
+    # Metadata & bibtex
+    mt <- here::here(path, nm)
+    masterwrite(meta, mt)
+    masterwrite(bib, mt)  
+    # _________________________________________________________________________________________ #
+  } #if exist clean, don't run again
+}

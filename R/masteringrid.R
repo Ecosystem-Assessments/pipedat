c731928cd1dfd26f90db063ec4dd81bf77dc481f
David Beauchesne <david.beauchesne@uqar.ca>
2022-12-08 16:05:52 -0500

    update masteringrid\

--- R/masteringrid.R
+++ R/masteringrid.R
@@ -19,12 +19,18 @@
 #' }
 #' @export
 masteringrid <- function(dat, grd = here::here("data/grid/grid.tif")) {
+  # Get grid
+  if (class(grd) == "character") grd <- stars::read_stars(grd)
+
   # stars objects
   if ("stars" %in% class(dat)) {
-    # Get grid
-    if (class(grd) == "character") grd <- stars::read_stars(grd)
-
-    # Integrate in grid
     stars::st_warp(dat, grd)
   }
+  
+  # # sf objects
+  # if ("sf" %in% class(dat)) {
+  #   exactextract::exact_extract() <- works with raster package 
+  #   fasterize::fasterize() <- works with raster package 
+  #   stars::st_rasterize() <- works directly with stars (obviously)
+  # }
 }

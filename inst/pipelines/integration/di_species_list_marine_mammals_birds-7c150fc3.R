9d010310358b0f716c30bbf20d97bbba86162e67
David Beauchesne <david.beauchesne@uqar.ca>
2023-01-05 11:21:00 -0500

    add marine mammals and birds list

---
+++ inst/pipelines/integration/di_species_list_marine_mammals_birds-7c150fc3.R
@@ -0,0 +1,88 @@
+#' @eval get_name("7c150fc3")
+#'
+#' @eval get_description("7c150fc3")
+#'
+#' @eval dp_params()
+#' @eval di_params()
+#' @param ... further arguments used in functions, if applicable.
+#'
+#' @family pipeline functions
+#' @rdname integration_pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: 7c150fc3
+#'
+#' @examples
+#' \dontrun{
+#' di_7c150fc3()
+#' }
+di_7c150fc3 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, grid = NULL, ...) {
+  # Output folders and other objects used
+  uid <- "7c150fc3"
+  nm <- glue::glue("{get_shortname(uid)}-{uid}")
+  exist <- check_files(uid)
+  path <- make_output(uid)
+
+  if (!exist$integrated) {
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # IMPORT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    raw_id <- get_rawid(uid) # String with data to import
+    pipedat(raw_id, bbox, bbox_crs, timespan)
+    dat <- importdat(raw_id)[[1]]
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # ANALYZE / FORMAT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    species <- dplyr::filter(
+      dat,
+      Class %in% c("Mammalia","Aves")
+    ) |>
+    dplyr::rename(aphiaID = AphiaID) |>
+    dplyr::select(
+      aphiaID,
+      ScientificName,
+      Kingdom,
+      Phylum,
+      Class,
+      Order,
+      Family,
+      Genus,
+      Species
+    )
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE METADATA
+    # WARNING: mandatory
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    meta <- get_metadata(
+      pipeline_type = "integration",
+      pipeline_id = uid,
+      integration_data = raw_id
+    )      
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE BIBTEX
+    # WARNING: mandatory
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    bib <- get_bib(uid)
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # EXPORT 
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Formatted data   
+    fm <- here::here(path,glue::glue("{nm}"))
+    # for(i in 1:length(dat)) masterwrite(dat[[i]], fm[i])
+    masterwrite(species, fm)
+    
+    # Metadata & bibtex
+    mt <- here::here(path, nm)
+    masterwrite(meta, mt)
+    masterwrite(bib, mt)  
+    # _________________________________________________________________________________________ #
+  }
+}

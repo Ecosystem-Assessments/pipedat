7e59918b0fc66853429424911e5ec9315cfdb358
David Beauchesne <david.beauchesne@uqar.ca>
2023-01-06 10:13:41 -0500

    oxygen st lawrence

---
+++ inst/pipelines/integration/di_bottom_oxygen_egsl-0d36cf5d.R
@@ -0,0 +1,81 @@
+#' @eval get_name("0d36cf5d")
+#'
+#' @eval get_description("0d36cf5d")
+#'
+#' @eval dp_params()
+#' @eval di_params()
+#' @param ... further arguments used in functions, if applicable.
+#'
+#' @family pipeline functions
+#' @rdname integration_pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: 0d36cf5d
+#'
+#' @examples
+#' \dontrun{
+#' di_0d36cf5d()
+#' }
+di_0d36cf5d <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, grid = NULL, ...) {
+  # Output folders and other objects used
+  uid <- "0d36cf5d"
+  nm <- glue::glue("{get_shortname(uid)}-{uid}")
+  exist <- check_files(uid)
+  path <- make_output(uid)
+
+  if (!exist$integrated) {
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # IMPORT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    raw_id <- get_rawid(uid) # String with data to import
+    pipedat(raw_id, bbox, bbox_crs, timespan)
+    dat <- importdat(raw_id) 
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # ANALYZE / FORMAT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    dat <- lapply(
+      dat,
+      function(x) {
+        y =sf::st_as_sf(x, coords = c("longitude","latitude"), crs = 4326) |>
+        sf::st_transform(32198) |>
+        stars::st_rasterize(dx = 10000, dy = 10000) |>
+        masteringrid() 
+      }
+    )
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE METADATA
+    # WARNING: mandatory
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    meta <- get_metadata(
+      pipeline_type = "integration",
+      pipeline_id = uid,
+      integration_data = raw_id
+    )
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE BIBTEX
+    # WARNING: mandatory
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    bib <- get_bib(uid)
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # EXPORT 
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Formatted data   
+    yr <- 2011:2020
+    fm <- here::here(path,glue::glue("{nm}-oxygen-{yr}"))
+    for(i in 1:length(dat)) masterwrite(dat[[i]][3], fm[i])
+    
+    # Metadata & bibtex
+    mt <- here::here(path, nm)
+    masterwrite(meta, mt)
+    masterwrite(bib, mt)  
+    # _________________________________________________________________________________________ #
+  }
+}

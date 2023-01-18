5c1cafbd116d7b4f6d5090200c471fc4261eaf7a
David Beauchesne <david.beauchesne@uqar.ca>
2023-01-18 06:21:10 -0500

    trophic sensitivity

---
+++ inst/pipelines/data/dp_species_trophic_sensitivity-0b902b1e.R
@@ -0,0 +1,78 @@
+#' @eval get_name("0b902b1e")
+#'
+#' @eval get_description("0b902b1e")
+#'
+#' @eval dp_params()
+#'
+#' @family pipeline functions
+#' @rdname data_pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: 0b902b1e
+#'
+#' @examples
+#' \dontrun{
+#' dp_0b902b1e()
+#' }
+dp_0b902b1e <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ...) {
+  # Output folders and other objects used
+  uid <- "0b902b1e"
+  nm <- glue::glue("{get_shortname(uid)}-{uid}")
+  exist <- check_files(uid)
+  path <- make_output(uid)
+
+    
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # DOWNLOAD DATA
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  if (!exist$raw) {
+    # If the data is downloaded from online sources
+    urls <- c(
+      "https://github.com/david-beauchesne/FoodWeb-MultiStressors/raw/master/Data/vulnerability.RData"
+    )    
+    # Load
+    pipeload(
+      urls = urls, 
+      output = here::here(path, "raw"), 
+      large = FALSE
+    )
+  }
+  # _________________________________________________________________________________________ #
+    
+  if (!exist$clean) {
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # IMPORT DATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    load(here::here(path, "raw", "vulnerability.RData"))
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE METADATA
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    meta <- get_metadata(
+      pipeline_type = "data",
+      pipeline_id = uid,
+      access = timestamp()
+    )
+    # _________________________________________________________________________________________ #
+
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # CREATE BIBTEX
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    bib <- get_bib(uid)
+    # _________________________________________________________________________________________ #
+    
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # EXPORT 
+    # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+    # Formatted data   
+    fm <- here::here(path,glue::glue("{nm}"))
+    masterwrite(vulnerability, fm)
+    
+    # Metadata & bibtex
+    mt <- here::here(path, nm)
+    masterwrite(meta, mt)
+    masterwrite(bib, mt)  
+    # _________________________________________________________________________________________ #
+  } #if exist clean, don't run again
+}

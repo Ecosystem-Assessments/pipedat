723813c41e422d1ca1db075c3dfd31f2a7b5697a
David Beauchesne <david.beauchesne@uqar.ca>
2023-05-16 11:01:19 -0400

    add canadian geographical places

---
+++ inst/pipelines/canadian_geographical_names-92230392.R
@@ -0,0 +1,88 @@
+#' @eval get_name("92230392")
+#'
+#' @eval get_description("92230392")
+#'
+#' @eval dp_params()
+#'
+#' @family pipeline functions
+#' @rdname pipelines
+#' @seealso \code{\link{pipedat}}
+#'
+#' @keywords pipeline_id: 92230392
+#'
+#' @examples
+#' \dontrun{
+#' dp_92230392()
+#' }
+dp_92230392 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ingrid = TRUE, keep_raw = TRUE, ...) {
+  uid <- "92230392"
+  nm <- glue::glue("{get_shortname(uid)}-{uid}")
+  path <- make_path(uid)
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # DOWNLOAD DATA
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  if (check_raw(uid)) {
+    # If the data is downloaded from online sources
+    urls <- c(
+      "https://ftp.cartes.canada.ca/pub/nrcan_rncan/vector/geobase_cgn_toponyme/prov_csv_eng/cgn_canada_csv_eng.zip"
+    )
+        
+    # Load
+    pipeload(
+      urls = urls, 
+      output = here::here(path, "raw"), 
+      large = FALSE
+    )
+  }
+  # _________________________________________________________________________________________ #    
+  
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Format data 
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  if (check_format(uid)) {
+    # Files
+    files <- here::here(path,"raw") |>
+             dir(full.names = TRUE, recursive = TRUE, pattern = ".csv$")
+    
+    # Import
+    dat <- masterload(files) |> 
+           sf::st_as_sf(coords = c("Longitude","Latitude"), crs = 4326)
+    
+    # Subset data (if specified by user)
+    # on.exit(sf::sf_use_s2(TRUE), add = TRUE)
+    # sf::sf_use_s2(FALSE)
+    # dat <- lapply(dat, dp_parameters, bbox = bbox, timespan = timespan)
+    dat <- dp_parameters(dat, bbox, timespan)
+
+    # Export
+    fm <- here::here(path,"format",glue::glue("{nm}"))
+    masterwrite(dat, fm)    
+  } 
+  # _________________________________________________________________________________________ #
+
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Metadata & bibtex
+  # =~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~=~-~= #
+  # Metadata
+  meta <- get_metadata(
+    pipeline_type = "data",
+    pipeline_id = uid,
+    pipeline_bbox = bbox, 
+    pipeline_timespan = timespan, 
+    access = timestamp()
+  )
+
+  # bibtex
+  bib <- get_bib(uid)
+
+  # Export
+  mt <- here::here(path, nm)
+  masterwrite(meta, mt)
+  masterwrite(bib, mt)  
+  write_pipeline(uid)
+
+  # Clean 
+  clean_path(uid, keep_raw)
+  # _________________________________________________________________________________________ #
+}

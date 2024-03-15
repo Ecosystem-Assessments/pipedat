a4684813d5f3262af7a96d5f5e53214f8a7299c2
David Beauchesne <david.beauchesne@uqar.ca>
2024-03-15 11:41:30 -0400

    script for istop data

--- inst/pipelines/neec_cws-77f8d683.R
+++ inst/pipelines/neec_cws-77f8d683.R
@@ -192,9 +192,9 @@ dp_77f8d683 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ingrid = 
     # Based on email exhange with Robert Ronconi on 2024-02-23
     # Keep all incidents within 1km of the coastline
     # Coastal outline
+    tmp <- here::here(path, "tmp")
+    chk_create(tmp)
     getdat <- function(country) {
-      tmp <- here::here(path, "tmp")
-      chk_create(tmp)
       raster::getData("GADM", country = country, level = 0, path = tmp) |>
         sf::st_as_sf() |>
         sf::st_simplify(dTolerance = 600, preserveTopology = FALSE) |>
@@ -216,6 +216,8 @@ dp_77f8d683 <- function(bbox = NULL, bbox_crs = NULL, timespan = NULL, ingrid = 
     iid <- sf::st_disjoint(terre_buf, neec) |> unlist()
     neec <- neec[iid, ]
 
+    # Remove downloaded data
+    fs::dir_delete(tmp)
     # ----------------------------------------------------------------
     # To explore:
     # Impact to Waterbody field.  Consider if we should treat “actual” vs. “potential” spills differently.  Data on “potential” spills only started being recorded in April 2021 onward (no differentiation before that).  Some “Potential” spills may report very large amounts of substance that was not actually spilled (e.g.  Incident Number NL-20200611-03045-20 was a disabled vessel with 400000 L of fuel on board, but nothing spilled).  These “Potential” spills still represent risk even if no true pollution occurred.
